84eebadd7d1675816eec2f3c86677b690d61f042
GitHub Action <action@github.com>
2022-07-18 02:17:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/snailupupsudo/jetbrainplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/snailupupsudo/jetbrainplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.snailupupsudo.jetbrainplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.snailupupsudo.jetbrainplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/snailupupsudo/jetbrainplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.snailupupsudo.jetbrainplugin.services
+
+import com.github.snailupupsudo.jetbrainplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
